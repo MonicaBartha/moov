f0bb1ee4a3d7044c88b44c90f6638d62e823e8e0
Corina <corinacvp@gmail.com>
2020-03-26 19:52:20 -0300

    view service

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import '../App.css';
+import '../assets/css/services.css';
 
 class Category extends Component {
     render() {
         return (
-            <div className='App'>
-                <h1>Componente de las categorías</h1>
+            <div>
+                <p className='titleServices'>Services</p>
+                <div className='sectionServices'>
+                    <div className='itemServices'>Restaurantes</div>
+                    <div className='itemServices'>Hoteles</div>
+                    <div className='itemServices'>Museos</div>
+                    <div className='itemServices'>Turismo Aventura</div>
+                    <div className='itemServices'>Playas y Balnearios</div>
+                    <div className='itemServices'>Parques</div>
+                    <div className='itemServices'>Otros</div>
+                </div>
             </div>
         );
     }
