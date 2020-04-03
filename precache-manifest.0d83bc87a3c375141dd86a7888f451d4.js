11de8a1b515448b1665e3d219be05f3e7a5f525d
MonicaBartha <barthamonica@gmail.com>
2020-04-02 21:52:18 -0300

    Updates

---
+++ precache-manifest.0d83bc87a3c375141dd86a7888f451d4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8bfb28ea303220387e3c9599df2257",
+    "url": "/MonicaBartha/moov/index.html"
+  },
+  {
+    "revision": "4bbdcd422d57e7aefa2c",
+    "url": "/MonicaBartha/moov/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "12f6341a91b3c0612adf",
+    "url": "/MonicaBartha/moov/static/css/main.ea730735.chunk.css"
+  },
+  {
+    "revision": "4bbdcd422d57e7aefa2c",
+    "url": "/MonicaBartha/moov/static/js/2.4683df15.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/MonicaBartha/moov/static/js/2.4683df15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f6341a91b3c0612adf",
+    "url": "/MonicaBartha/moov/static/js/main.fe2c2e2e.chunk.js"
+  },
+  {
+    "revision": "fa38cb63a0a1195761b6",
+    "url": "/MonicaBartha/moov/static/js/runtime-main.98c0cfc4.js"
+  },
+  {
+    "revision": "0bb83656f0e4f457b2c935989c733d67",
+    "url": "/MonicaBartha/moov/static/media/home-city.0bb83656.jpg"
+  },
+  {
+    "revision": "cbd91ba6a4679ca065d0f782291b16ad",
+    "url": "/MonicaBartha/moov/static/media/home-lights.cbd91ba6.jpg"
+  },
+  {
+    "revision": "d342bdb4cc2c97dc6c8da6c8f453cc13",
+    "url": "/MonicaBartha/moov/static/media/home-sunrise.d342bdb4.jpg"
+  },
+  {
+    "revision": "483ca8f2c7c6dec191a77c3b8f582b2f",
+    "url": "/MonicaBartha/moov/static/media/home-transporte.483ca8f2.jpg"
+  },
+  {
+    "revision": "d9a24d05eed3db4d122adcaf0ac3d956",
+    "url": "/MonicaBartha/moov/static/media/iconAcercaDe.d9a24d05.svg"
+  },
+  {
+    "revision": "952237ec8582c999ac5afe86cf504fd1",
+    "url": "/MonicaBartha/moov/static/media/iconAvion.952237ec.svg"
+  },
+  {
+    "revision": "d9d4427be3712f84989f4fbd31a58161",
+    "url": "/MonicaBartha/moov/static/media/iconBack.d9d4427b.svg"
+  },
+  {
+    "revision": "71152490836efb997681f2463655bf53",
+    "url": "/MonicaBartha/moov/static/media/iconFavoritos.71152490.svg"
+  },
+  {
+    "revision": "fa2c07daf87d1ec998638f37e1412080",
+    "url": "/MonicaBartha/moov/static/media/iconHome.fa2c07da.svg"
+  },
+  {
+    "revision": "f889f960c3a3c7488bb507f3466d9b06",
+    "url": "/MonicaBartha/moov/static/media/iconMapa.f889f960.svg"
+  },
+  {
+    "revision": "22f8154d1e6adba70936c777099c869b",
+    "url": "/MonicaBartha/moov/static/media/iconServicios.22f8154d.svg"
+  },
+  {
+    "revision": "53cbb655c4afb5a66fd53de9b72df4c0",
+    "url": "/MonicaBartha/moov/static/media/location-mark.53cbb655.png"
+  },
+  {
+    "revision": "e568f3ca2b2fada8fb192af1c6e733b4",
+    "url": "/MonicaBartha/moov/static/media/logoMoov.e568f3ca.png"
+  },
+  {
+    "revision": "d214da21056050d5ce7f5ae6ab61e571",
+    "url": "/MonicaBartha/moov/static/media/moov.d214da21.png"
+  },
+  {
+    "revision": "abc323cc5bc9a95b21de94ae418b83d2",
+    "url": "/MonicaBartha/moov/static/media/museos.abc323cc.jpg"
+  },
+  {
+    "revision": "78bf21dc03a6f48590198057b51a7564",
+    "url": "/MonicaBartha/moov/static/media/servicios-hoteles.78bf21dc.jpg"
+  },
+  {
+    "revision": "8e99945e12da989edecc4804bf42a079",
+    "url": "/MonicaBartha/moov/static/media/servicios-parques.8e99945e.jpg"
+  },
+  {
+    "revision": "8fbf613f1a1c6459140cb9054a8511a7",
+    "url": "/MonicaBartha/moov/static/media/servicios-playas.8fbf613f.jpg"
+  },
+  {
+    "revision": "6e586ade08b4858a72668290804cc1d8",
+    "url": "/MonicaBartha/moov/static/media/servicios-restaurantes.6e586ade.jpg"
+  },
+  {
+    "revision": "d6e3d5e4e9ea925ad67cfdf1daf1b96a",
+    "url": "/MonicaBartha/moov/static/media/servicios-turismo-aventura.d6e3d5e4.jpg"
+  },
+  {
+    "revision": "bb7daa97874804191be47e5cd858409e",
+    "url": "/MonicaBartha/moov/static/media/times-circle-solid.bb7daa97.svg"
+  },
+  {
+    "revision": "34f126497c9f8dc536a0f024f90791b8",
+    "url": "/MonicaBartha/moov/static/media/welcome-beach-mobile.34f12649.jpg"
+  },
+  {
+    "revision": "61011dcb2fd2ab9d8d59c32847286d3a",
+    "url": "/MonicaBartha/moov/static/media/welcome-beach.61011dcb.jpg"
+  }
+]);
